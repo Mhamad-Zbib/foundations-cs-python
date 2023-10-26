ab5be84b0070c4206c507e58c5bb86e8852d1452
Mhamad-Zbib <mzbib.2001@gmail.com>
2023-10-26 20:37:09 +0300

    PartB

---
+++ Assignment3.py
@@ -0,0 +1,12 @@
+def displayMenu():
+    name = input("Enter your name: ")
+    print("Welcome", name)
+    print(
+        "1. Add Matrices\n"
+        + "2. Check Rotation\n"
+        + "3. Invert Dictionary\n"
+        + "4. Convert Matrix to Dictionary\n"
+        + "5. Check Palindrome\n"
+        + "6. Search for an Element & Merge Sort\n"
+        + "7.Exit"
+    )
