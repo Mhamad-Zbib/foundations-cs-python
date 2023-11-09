bfc0a3f2e3966a6bf93ab176e01278f821962d3c
Mhamad-Zbib <mzbib.2001@gmail.com>
2023-11-09 21:36:48 +0200

    open tab function

--- Midterm.py
+++ Midterm.py
@@ -2,16 +2,27 @@ print("------------")
 name = input("Enter you name: ")
 print(f"Welcome to the Browser Tabs Simulation {name.capitalize()}!")
 
+
 def displayMenu():
-    # The menu is here to display what options the user can pick from
-    print("The menu:\n"
-          "     1. Open Tab \n"
-          "     2. Close Tab \n"
-          "     3. Switch Tab \n"
-          "     4. Display All Tabs \n"
-          "     5. Open Nested Tab \n"
-          "     6. Clear All Tabs \n"
-          "     7. Save Tabs \n"
-          "     8. Import Tabs \n"
-          "     9. Exit")
-    
+    # The menu is here to display what options the user can pick.
+    print(
+        "The menu:\n"
+        "     1. Open Tab \n"
+        "     2. Close Tab \n"
+        "     3. Switch Tab \n"
+        "     4. Display All Tabs \n"
+        "     5. Open Nested Tab \n"
+        "     6. Clear All Tabs \n"
+        "     7. Save Tabs \n"
+        "     8. Import Tabs \n"
+        "     9. Exit"
+    )
+
+
+tab = []
+
+
+def openTab():
+    title = input("Enter the title of the website: ")
+    url = input("Enter the url of the website: ")
+    tab.append({"title": title, "url": url})
