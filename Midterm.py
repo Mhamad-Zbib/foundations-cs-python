2e5ca32d4ec718eadaebb04f9e1651aaf670a82f
Mhamad-Zbib <mzbib.2001@gmail.com>
2023-11-09 20:35:52 +0200

    Menu

--- Midterm.py
+++ Midterm.py
@@ -1,4 +1,17 @@
 print("------------")
 name = input("Enter you name: ")
-print(f"Welcome to your Browser Tabs Simulation {name.capitalize}")
+print(f"Welcome to the Browser Tabs Simulation {name.capitalize()}!")
 
+def displayMenu():
+    # The menu is here to display what options the user can pick from
+    print("The menu:\n"
+          "     1. Open Tab \n"
+          "     2. Close Tab \n"
+          "     3. Switch Tab \n"
+          "     4. Display All Tabs \n"
+          "     5. Open Nested Tab \n"
+          "     6. Clear All Tabs \n"
+          "     7. Save Tabs \n"
+          "     8. Import Tabs \n"
+          "     9. Exit")
+    
