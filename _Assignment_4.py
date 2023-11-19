8f81a82d427c20aab1b1dfb6f8ffa75432462d65
Mhamad-Zbib <mzbib.2001@gmail.com>
2023-11-19 21:08:04 +0200

    First Display Menu

--- _Assignment_4.py
+++ _Assignment_4.py
@@ -1,2 +1,14 @@
-name  = input("Enter your name: ")
+name = input("Enter your name: ")
 print(f"Welcome {name.capitalize()}!")
+
+
+def displayMenu():
+    print(
+        "the menu: \n"
+        "       1. Singly Linked List \n"
+        "       2. Check if Palindrome \n"
+        "       3. Priority Queue \n"
+        "       4. Evalualte an Infix Expression \n"
+        "       5. Graph \n"
+        "       6. Exit \n"
+    )
