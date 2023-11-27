8593ad38bca8dc45639512bd15363e294499cc89
Mhamad-Zbib <mzbib.2001@gmail.com>
2023-11-27 14:42:08 +0200

    Adding comments and Big O

--- _Assignment_4.py
+++ _Assignment_4.py
@@ -2,7 +2,8 @@ name = input("Enter your name: ")
 print(f"Welcome {name.capitalize()}!")
 
 
-def displayMenu():
+def displayMenu(): # O(1)
+    # The Main Menu
     print(
         "the menu: \n"
         "       1. Singly Linked List \n"
@@ -24,7 +25,9 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def addNode(self, value, position):
+    def addNode(self, value, position): # O(N), N being the length of the Nodes.
+        # In this LinkedList, the user is able to add a node and if the node is empty then the head will point to the new node.
+        # And if the user enters a position out of bounds it will add it to the end of the LL.
         node = Node(value)
         if self.head is None:
             self.head = node
@@ -49,14 +52,16 @@ class LinkedList:
             current.next = node
             print(f"Added node {value} at the end of the LinkedList, since your position is out of bounds.\n")
 
-    def displayNodes(self):
+    def displayNodes(self): # O(N), N being the length of the Nodes.
+        # The user is able to display the nodes.
         current = self.head
         while current is not None:
             print(current.info, end=" => ")
             current = current.next
         print("None")
 
-    def removeNode(self, value):
+    def removeNode(self, value): # O(N), N being the length of the Nodes.
+        # In this function after the user insert the value he wants to remove the function will search for thev value and remove it. However, it will only remove the value once.
         current = self.head
         previous = None
 
@@ -83,16 +88,20 @@ class LinkedList:
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self): # O(1)
+        # Initializing an empty list.
         self.list = []
 
-    def push(self, data):
+    def push(self, data): # O(1)
+        # Appending elemetns insite the list.
         self.list.append(data)
 
-    def isEmpty(self):
+    def isEmpty(self): # O(1)
+        # Check if the list is empty
         return self.list == []
 
-    def pop(self):
+    def pop(self): # O(1)
+        # Remove the last element from the list if the list is not empty.
         if not self.isEmpty():
             return self.list.pop()
         else:
@@ -107,6 +116,7 @@ class Node1:
 
 class Student:
     def __init__(self, name, midterm, final, attitude):
+        # Initializing the student class.
         self.name = name
         self.midterm = midterm
         self.final = final
@@ -118,7 +128,10 @@ class PriorityQueue:
         self.head = None
         self.size = 0
 
-    def addStudent(self, student):
+    def addStudent(self, student): # O(N), N being the length of the Node.
+        # add a student to the priority queue, at first will interview the student with good attitude.
+        # Second the student with the highest final grade.
+        # Third the student with the highest midterm grade.
         node = Node1(student)
         if self.head == None:
             self.head = node
@@ -163,7 +176,8 @@ class PriorityQueue:
                 previous.next = node
             self.size += 1
 
-    def interview(self):
+    def interview(self): # O(1)
+        # In this functon, the HR will interview the students. Based on the size of the Priority Queue(students) and by order.
         if self.size > 1:
             print(f"You have {self.size} more students to interview.")
             print(f"You have an interview now with: {self.head.student.name}\n")
@@ -181,7 +195,8 @@ class PriorityQueue:
 
 
 class InfixExpression:
-    def operation(self, op, b, a):
+    def operation(self, op, b, a): # O(1)
+        # In this function, we let each operator to perform operations on values.
         if op == "+":
             return a + b  
         elif op == "-":
@@ -191,7 +206,8 @@ class InfixExpression:
         elif op == "/":
             return a / b
 
-    def priority(self, op):
+    def priority(self, op): # O(1)
+        # Here we give priority based on the order of operations PEMDAS.
         if op == "*" or op == "/":
             return 2
         elif op == "+" or op == "-":
@@ -201,7 +217,8 @@ class InfixExpression:
         else:
             return -1
 
-    def evaluateString(self, expression):
+    def evaluateString(self, expression): # O(N), N Being the length of the list
+        # In this function we use 2 stacks to store operators and numbers and then the function will evaluate them at the end.
         values = []  
         stack = []  
         i = 0
@@ -235,17 +252,20 @@ class InfixExpression:
 
 class Graph:
     def __init__(self, num_vertices):
+        # In this graph i used the adjacency matrix since the user is allowed to insert as many edges as he wants and to make the graph dense.
         self.num_vertices = num_vertices
         self.adj_matrix = [[0] * num_vertices for _ in range(num_vertices)]
 
-    def addVertex(self):
+    def addVertex(self): # O(V). 
+        # This function permits the user to add a vertex(row).
         self.num_vertices += 1
         for row in self.adj_matrix:
             row.append(0)
         self.adj_matrix.append([0] * self.num_vertices)
         print("Added vertex", self.num_vertices, "\n")
 
-    def addEdge(self, v1, v2):
+    def addEdge(self, v1, v2): # O(1)
+        # In here the function will add an edge between to vertices.
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             self.adj_matrix[v1][v2] = 1
             self.adj_matrix[v2][v1] = 1
@@ -261,7 +281,8 @@ class Graph:
             print(f"Invalid Vertex: {v2}")
 
 
-    def removeVertex(self, v):
+    def removeVertex(self, v): # O(V).
+        # The function will remove the vertex and all edges connected to it.
         if v < 0 or v >= self.num_vertices:
             print("Invalid index to remove.")
             return 
@@ -271,7 +292,8 @@ class Graph:
             i.pop(v)
         print("Vertex removed with all edges connected to it.\n")
 
-    def removeEdge(self, v1, v2):
+    def removeEdge(self, v1, v2): # O(1)
+        # in here the user will remove an edge between to vertices.
         if self.adj_matrix[v1][v2] != 0 and self.adj_matrix[v2][v1] != 0:
             self.adj_matrix[v1][v2] = 0
             self.adj_matrix[v2][v1] = 0
@@ -279,7 +301,8 @@ class Graph:
         else:
             print(f"There is no edge between these vertices: {v1} and {v2}\n")
 
-    def displayGraph(self, d):
+    def displayGraph(self, d): # O(V).
+        # In here the function will display the graph based on the degree he inserts, all vertices containing that degree will be displayed.
         if len(self.adj_matrix) == 0:
             print("Graph is empty!\n")
             return
@@ -290,7 +313,8 @@ class Graph:
 
 
 
-def first_choice_menu():
+def first_choice_menu(): # O(1)
+    # Menu of the Linked List
     print(
         "the menu: \n"
         "       a. Add Node \n"
@@ -299,7 +323,8 @@ def first_choice_menu():
         "       d. Return to main menu \n"
     )
 
-def fifth_choice_menu():
+def fifth_choice_menu(): # O(1)
+        # Menu of the graph
         print(
         "the menu: \n"
         "       a. Add Vertex \n"
@@ -312,7 +337,7 @@ def fifth_choice_menu():
         
 
 
-def main():
+def main(): # O(N^2), N being the size of input.
     choice = 0
     consecutive_errors = 0
     while choice != 6:
@@ -327,7 +352,7 @@ def main():
             print(" - Try again and enter a valid number.\n")
             consecutive_errors += 1
         elif int(choice) < 1 or int(choice) > 6:
-            print(" - Try again and choose number between 1 and 6.\n")
+            print(" - Try again and choose a number between 1 and 6.\n")
             consecutive_errors += 1
         else:
             consecutive_errors = 0
@@ -392,12 +417,12 @@ def main():
 
             elif int(choice) == 3:
                 pq = PriorityQueue()
-                num = int(input("Enter how many students: "))
+                num = int(input(" - Enter how many students: "))
                 for i in range(num):
-                    name = input("Enter the name of the student: ")
-                    midterm = int(input("Enter the midterm grade of the student: "))
-                    final = int(input("Enter the final grade of the student: "))
-                    attitude = input("Enter the attitude of the student (True or False): ")
+                    name = input(" - Enter the name of the student: ")
+                    midterm = int(input(" - Enter the midterm grade of the student: "))
+                    final = int(input(" - Enter the final grade of the student: "))
+                    attitude = input(" - Enter the attitude of the student (True or False): ")
                     print()
                     if attitude.lower() == "true":
                         attitude1 = True
@@ -408,14 +433,14 @@ def main():
                         student = Student(name.capitalize(), midterm, final, attitude2)
                         pq.addStudent(student)
                     else:
-                        print("Try again and enter 'True' or 'False' for attitude.")
+                        print(" - Try again and enter 'True' or 'False' for attitude.")
                 pq.interview()
 
             elif int(choice) == 4:
                 infix = InfixExpression()
-                expression = input("Enter your expression: ")
+                expression = input(" - Enter your expression: ")
                 result = infix.evaluateString(expression)
-                print(f"Result: {result}")
+                print(f" - Result: {result}")
 
             elif int(choice) == 5:
                 num = input(" - Enter the number of vertices: ")
@@ -436,31 +461,31 @@ def main():
                             graph.addVertex()
 
                         elif option.lower() == "b":
-                            v1 = int(input("Enter the first vertex: "))
-                            v2 = int(input("Enter the second vertex: "))
+                            v1 = int(input(" - Enter the first vertex: "))
+                            v2 = int(input(" - Enter the second vertex: "))
                             print()
                             graph.addEdge(v1, v2)
 
                         elif option.lower() == "c":
-                            v = int(input("Enter the vertex to remove: "))
+                            v = int(input(" - Enter the vertex to remove: "))
                             print()
                             graph.removeVertex(v)
                         
                         elif option.lower() == "d":
-                            v1 = int(input("Enter the first vertex: "))
-                            v2 = int(input("Enter the second vertex: "))
+                            v1 = int(input(" - Enter the first vertex: "))
+                            v2 = int(input(" - Enter the second vertex: "))
                             print()
                             graph.removeEdge(v1, v2)
 
                         elif option.lower() == "e":
-                            degree = int(input("Enter the minimum degree to display: "))
+                            degree = int(input(" - Enter the minimum degree to display: "))
                             graph.displayGraph(degree)
 
                         elif option.lower() == "f":
                             break
 
                         else:
-                            print("Invalid Choice. Enter a valid option.")
+                            print(" - Invalid Choice. Enter a valid option.")
 
             elif int(choice) == 6:
                 break
